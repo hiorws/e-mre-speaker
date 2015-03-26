e0e2e5b1bff902d01647e7649b81e47f6f965b88
Ozgur <ozgurfiratcinar@gmail.com>
2015-03-27 00:14:53 +0200

    Create espeak.py

---
+++ espeak.py
@@ -0,0 +1,17 @@
+___author___ = 'Ozgur'
+import random
+
+
+print ("###e-Mre Speaker v1.0###")
+
+speakList = ['Kankii', 'Kankii', 'Kankiiii', 'Dota', 'Prophet', 'Panda',
+ 			'Kankii bunlarla da kreplere yatarsan adam degilsin kankii.',
+ 			'Kanki naptın?', 'Faynayt sıktı be kankii.', 
+ 			'Olm ben o adamla bira içtim.', 'Aynen kanki aynen.', 'aynen',
+ 			 'aynen', 'aynen', 'aynen', 'aynen', 'aynen kanki aynen', 'aynen',
+ 			 'aynen']
+
+
+for i in speakList:
+	print (i)
+	input()
