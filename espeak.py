922b2fd71852394804e084dc0bf7118fd452a7f2
Ozgur Firat Cinar <ozgurfiratcinar@gmail.com>
2016-03-04 20:08:20 +0200

    edited espeak.py

--- espeak.py
+++ espeak.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-___author___ = 'ozgur'
+___author___ = 'hiorws'
 
 from gtts import gTTS
 
-speakList = ['Kankaa', 'Kankaa', 'Kanka devreyi ne yaptın?', 'Dota Kanka', 'Prophet', 'Panda',
+speak_list = ['Kankaa', 'Kankaa', 'Kanka devreyi ne yaptın?', 'Dota Kanka', 'Prophet', 'Panda',
  			'Kanki bunlarla da kreplere yatarsan adam degilsin kanki.',
- 			'Kanki naptın?', 'Faynayt sıktı be kanki.', 
+ 			'Kanki naptın?', 'Faynayt sıktı be kanki.',
  			'Aynen kanka.', 'Aynen kanki aynen.', 'aynen',
  			 'aynen', 'aynen', 'aynen', 'aynen', 'aynen kanki aynen', 'aynen',
  			 'aynen']
 
 tts = gTTS(text=' '.join(speakList), lang='tr')
 
-tts.save("emrebot4.mp3")
+tts.save("emrebot.mp3")
